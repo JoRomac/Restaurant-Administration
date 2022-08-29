4c0efb95ed2d737e0dcf9f90455afe39a6211860
Josip Romac <josipromac@gmail.com>
2022-08-30 00:48:26 +0200

    Resource file dodan naknadno

--- BookingManager/resource.h
+++ BookingManager/resource.h
@@ -7,21 +7,31 @@
 #define IDD_MAIN_DLG                    130
 #define IDS_USER_UNKNOWN                132
 #define IDD_BOOKING_DIALOG              133
-#define IDS_TITLE_CONTACT               139
+#define IDD_NEW_RESERVATION             135
 #define IDC_USER                        1000
 #define IDC_PASSWORD                    1001
 #define IDSIGNOUT                       1002
 #define IDC_BUTTON1                     1003
 #define IDC_BUTTON2                     1004
 #define IDC_LIST1                       1006
+#define IDC_ADD_RESERVATION             1008
+#define IDC_NAME                        1009
+#define IDC_LASTNAME                    1010
+#define IDC_PAX                         1011
+#define IDC_OCCASION                    1018
+#define IDC_CONTACT                     1019
+#define IDC_TABLENUM                    1020
+#define IDC_TIME                        1021
+#define IDC_DATE                        1022
+#define IDC_DISPLAY_ALL                 1024
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_RESOURCE_VALUE        137
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1025
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
